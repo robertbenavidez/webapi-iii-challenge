600d747d2cb96a1b60119da2045c3ba258f2c017
Robert Benavidez <murabitbenavidez@gmail.com>
2019-10-31 10:46:56 -0400

    logger

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  const method = req.method;
+  const url = req.url;
+  const timestamp = moment().format("MMMM Do YYYY, h:mm:ss a"); 
 
+  console.log(`you made a ${method} request to ${url} at ${timestamp}`);
+  next();
 };
 
 module.exports = server;
